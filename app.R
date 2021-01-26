189db55097b58d61cf14b6d3e8869f9c41a865cf
Tran Doan Khanh Vu <tdkhanhvu@gmail.com>
2021-01-26 13:44:25 -0800

    Finish wrangling data

--- app.R
+++ app.R
@@ -16,9 +16,16 @@ library(tidyverse)
 
 # 1.5: Load country code data
 load_country_code <- function() {
-    country_code <- read_csv("data/country_location.csv")
+    country_code_data <- read_csv("data/country_location.csv")
     
-    country_code
+    country_code_data <- country_code_data %>%
+        rename(country_region = "country") %>%
+        select(-country_code)
+    
+    country_code_data$country_region <- country_code_data$country_region %>%
+        as.factor()
+    
+    country_code_data
 }
 
 # 1.6: Other supporting functions
@@ -28,25 +35,76 @@ load_country_code <- function() {
 
 # 2.1: Load from the file
 daily_data <- read_csv("data/raw/full_grouped.csv")
-head(daily_data)
+# head(daily_data)
 
 population_data <- read_csv("data/processed/worldometer_data.csv")
-print(head(population_data))
+# print(head(population_data))
 
 country_code_data <- load_country_code()
-print(head(country_code_data))
+# print(head(country_code_data))
 
-# 2.2: Replace mismatched values
+# 2.2: Rename columns
+daily_data <- daily_data %>%
+    rename(country_region = "Country/Region",
+           new_cases = "New cases",
+           new_deaths = "New deaths",
+           new_recovered = "New recovered",
+           who_region = "WHO Region",
+           date = "Date",
+           confirmed = "Confirmed",
+           deaths = "Deaths",
+           recovered = "Recovered",
+           active = "Active")
 
+population_data <- population_data %>%
+    rename(country_region = "Country/Region",
+           population = "Population")
 
-# 2.3: Convert `Country/Region` and `WHO Region` to factors
+# 2.3 Convert `Country/Region` and `WHO Region` to factors
+daily_data$country_region <- daily_data$country_region %>%
+    as.factor()
+
+daily_data$who_region <- daily_data$who_region %>%
+    as.factor()
+
+population_data$country_region <- population_data$country_region %>%
+    as.factor()
 
 # 3: Data wrangling
 
 # 3.1: Join
+country_daywise_df <- left_join(daily_data, population_data)
+country_daywise_df <- left_join(country_daywise_df, country_code_data)
+print(head(country_daywise_df))
 
 # 3.2: Aggregate into world / regions 
+region_daywise_df <- country_daywise_df %>%
+    group_by(date, who_region) %>%
+    summarize(confirmed = mean(confirmed),
+              deaths = mean(deaths),
+              recovered = mean(recovered),
+              active = mean(active),
+              new_cases = mean(new_cases),
+              new_deaths = mean(new_deaths),
+              new_recovered = mean(new_recovered)) %>%
+    ungroup() %>%
+    mutate(country_region = who_region)
+    
+# print(head(region_daywise_df))
 
+world_daywise_df <- country_daywise_df %>%
+    group_by(date) %>%
+    summarize(confirmed = mean(confirmed),
+              deaths = mean(deaths),
+              recovered = mean(recovered),
+              active = mean(active),
+              new_cases = mean(new_cases),
+              new_deaths = mean(new_deaths),
+              new_recovered = mean(new_recovered)) %>%
+    ungroup() %>%
+    mutate(country_region = "World")
+
+print(head(world_daywise_df))
 
 # 4: Declare objects
 
