9a8f8e58daf92f93abc1599a276d902a7c17988d
Tran Doan Khanh Vu <tdkhanhvu@gmail.com>
2021-01-29 05:26:59 -0800

    Modify files for Heroku deployment

--- init.R
+++ init.R
@@ -1,3 +1,7 @@
+# R script to run author supplied code, typically used to install additional R packages
+# contains placeholders which are inserted by the compile script
+# NOTE: this script is executed in the chroot context; check paths!
+
 r <- getOption('repos')
 r['CRAN'] <- 'http://cloud.r-project.org'
 options(repos=r)
@@ -5,6 +9,5 @@ options(repos=r)
 # ======================================================================
 
 # packages go here
-install.packages('remotes')
-
-remotes::install_github('plotly/dashR', upgrade=TRUE)
+install.packages(c('dash', 'readr', 'here', 'ggthemes', 'remotes'))
+remotes::install_github('facultyai/dash-bootstrap-components@r-release')
