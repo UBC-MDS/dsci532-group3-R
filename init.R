f5c4bd0952f499b28d4a9492854e97752cf17ad2
Tran Doan Khanh Vu <tdkhanhvu@gmail.com>
2021-01-29 05:18:38 -0800

    Add files for Heroku deployment

---
+++ init.R
@@ -0,0 +1,10 @@
+r <- getOption('repos')
+r['CRAN'] <- 'http://cloud.r-project.org'
+options(repos=r)
+
+# ======================================================================
+
+# packages go here
+install.packages('remotes')
+
+remotes::install_github('plotly/dashR', upgrade=TRUE)
